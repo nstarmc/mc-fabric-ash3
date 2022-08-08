822762be95e57dfb13070b8ec9656e076b7e3bc8
pcs <code@miraisoft.de>
2022-08-08 18:57:19 +0200

    commands + homepage fixed, new version 1.1

--- src/main/java/de/miraisoft/ash2/AshCommands.java
+++ src/main/java/de/miraisoft/ash2/AshCommands.java
@@ -1,116 +1,104 @@
 package de.miraisoft.ash2;
 
-import com.mojang.brigadier.CommandDispatcher;
+import com.mojang.brigadier.arguments.IntegerArgumentType;
 
 import de.miraisoft.ash2.config.AshConfig;
 import me.shedaniel.autoconfig.AutoConfig;
 import net.fabricmc.fabric.api.client.command.v2.ClientCommandManager;
-import net.fabricmc.fabric.api.client.command.v2.FabricClientCommandSource;
+import net.fabricmc.fabric.api.client.command.v2.ClientCommandRegistrationCallback;
 
 public class AshCommands {
+	
+	private static final String COMMAND_TOGGLE = "ash2toggle";
+	private static final String COMMAND_RESET = "ash2reset";
+	private static final String COMMAND_FPS = "ash2togglefps";
+	private static final String COMMAND_COORDS = "ash2togglecoords";
+	private static final String COMMAND_DIRECTION = "ash2toggledirection";
+	private static final String COMMAND_COLOR = "ash2color";
+	private static final String COMMAND_LIGHTLEVEL = "ash2togglelightlevel";
+	private static final String COMMAND_CONCISE_COORDS = "ash2toggleconcisecoords";
 
     public static AshConfig config;
 
 	public static void registerCommands() {
 
-		CommandDispatcher<FabricClientCommandSource> commandDispatcher = ClientCommandManager.getActiveDispatcher();
-
 		if (config == null) {
 			config = AutoConfig.getConfigHolder(AshConfig.class).getConfig();
 		}
 
-		if (commandDispatcher == null) {
-			System.out.println("Command Dispatcher is null");
-			return;
-		}
+		ClientCommandRegistrationCallback.EVENT.register((dispatcher, registryAccess) -> {
+			dispatcher.register(ClientCommandManager.literal(COMMAND_TOGGLE).executes(context -> {
+				config.showHud = !config.showHud;
+				AshMod.configManager.save();
+				return 1;
+			}));
+		});
 
-		commandDispatcher.register(ClientCommandManager.literal("toggleash2").executes(context -> {
-			config.showHud = !config.showHud;
-			AshMod.configManager.save();
-			return 1;
-		}));
+		ClientCommandRegistrationCallback.EVENT.register((dispatcher, registryAccess) -> {
+			dispatcher.register(ClientCommandManager.literal(COMMAND_RESET).executes(context -> {
+				config = new AshConfig();
+				AshMod.configManager.save();
+				return 1;
+			}));
+		});
+		
+		ClientCommandRegistrationCallback.EVENT.register((dispatcher, registryAccess) -> {
+			dispatcher.register(ClientCommandManager.literal(COMMAND_FPS).executes(context -> {
+				config.showFps = !config.showFps;
+				AshMod.configManager.save();
+				return 1;
+			}));
+		});
 
-		commandDispatcher.register(ClientCommandManager.literal("resetash2").executes(context -> {
-			config = new AshConfig();
-			AshMod.configManager.save();
-			return 1;
-		}));
-
+		ClientCommandRegistrationCallback.EVENT.register((dispatcher, registryAccess) -> {
+			dispatcher.register(ClientCommandManager.literal(COMMAND_COORDS).executes(context -> {
+                config.showCoords = !config.showCoords;
+                AshMod.configManager.save();
+                return 1;
+            }));
+		});
         
-//        commandDispatcher.register(ClientCommandManager.literal("togglefps")
-//            .executes(context -> {
-//                config.showFps = !config.showFps;
-//                AshMod.configManager.save();
-//                return 1;
-//            }));
-//
-//        commandDispatcher.register(ClientCommandManager.literal("togglecoords")
-//            .executes(context -> {
-//                config.showCoords = !config.showCoords;
-//                AshMod.configManager.save();
-//                return 1;
-//            }));
-//
-//        commandDispatcher.register(ClientCommandManager.literal("toggledirection")
-//            .executes(context -> {
-//                config.showDirection = !config.showDirection;
-//                AshMod.configManager.save();
-//                return 1;
-//            }));
-//
-//        commandDispatcher.register(ClientCommandManager.literal("ashcolor")
-//            .then(ClientCommandManager.argument("r", IntegerArgumentType.integer())
-//                    .then(ClientCommandManager.argument("g", IntegerArgumentType.integer())
-//                            .then(ClientCommandManager.argument("b", IntegerArgumentType.integer())
-//                                    .executes(context -> {
-//                                        int r = IntegerArgumentType.getInteger(context,"r");
-//                                        int g = IntegerArgumentType.getInteger(context,"g");
-//                                        int b = IntegerArgumentType.getInteger(context,"b");
-//
-//                                        config.hudColor = b + (g << 8) + (r << 16);
-//                                        AshMod.configManager.save();
-//                                        return 1;
-//                                    })))));
-//
-//        commandDispatcher.register(ClientCommandManager.literal("alignash")
-//            .then(ClientCommandManager.literal("left")
-//                    .executes(context -> {
-//                        config.align = 0;
-//                        AshMod.configManager.save();
-//                        return 1;
-//                    }))
-//            .then(ClientCommandManager.literal("center")
-//                    .executes(context -> {
-//                        config.align = 1;
-//                        AshMod.configManager.save();
-//                        return 1;
-//                    }))
-//            .then(ClientCommandManager.literal("right")
-//                    .executes(context -> {
-//                        config.align = 2;
-//                        AshMod.configManager.save();
-//                        return 1;
-//                    })));
-//
-//        commandDispatcher.register(ClientCommandManager.literal("valignash")
-//            .then(ClientCommandManager.literal("top")
-//                    .executes(context -> {
-//                        config.verticalAlign = 0;
-//                        AshMod.configManager.save();
-//                        return 1;
-//                    }))
-//            .then(ClientCommandManager.literal("middle")
-//                    .executes(context -> {
-//                        config.verticalAlign = 1;
-//                        AshMod.configManager.save();
-//                        return 1;
-//                    }))
-//            .then(ClientCommandManager.literal("bottom")
-//                    .executes(context -> {
-//                        config.verticalAlign = 2;
-//                        AshMod.configManager.save();
-//                        return 1;
-//                    })));
+		ClientCommandRegistrationCallback.EVENT.register((dispatcher, registryAccess) -> {
+			dispatcher.register(ClientCommandManager.literal(COMMAND_DIRECTION)
+	            .executes(context -> {
+                config.showDirection = !config.showDirection;
+                AshMod.configManager.save();
+                return 1;
+            }));
+		});
+		
+		ClientCommandRegistrationCallback.EVENT.register((dispatcher, registryAccess) -> {
+			dispatcher.register(ClientCommandManager.literal(COMMAND_COLOR)
+	            .then(ClientCommandManager.argument("r", IntegerArgumentType.integer())
+	                .then(ClientCommandManager.argument("g", IntegerArgumentType.integer())
+	                	.then(ClientCommandManager.argument("b", IntegerArgumentType.integer())
+	                		.executes(context -> {
+	                			int r = IntegerArgumentType.getInteger(context,"r");
+	                			int g = IntegerArgumentType.getInteger(context,"g");
+	                			int b = IntegerArgumentType.getInteger(context,"b");
+
+	                			config.hudColor = b + (g << 8) + (r << 16);
+	                			AshMod.configManager.save();
+	                			return 1;
+	                		})))));
+		});
         
+		ClientCommandRegistrationCallback.EVENT.register((dispatcher, registryAccess) -> {
+			dispatcher.register(ClientCommandManager.literal(COMMAND_LIGHTLEVEL)
+	            .executes(context -> {
+                config.showLightLevel = !config.showLightLevel;
+                AshMod.configManager.save();
+                return 1;
+            }));
+		});
+		
+		ClientCommandRegistrationCallback.EVENT.register((dispatcher, registryAccess) -> {
+			dispatcher.register(ClientCommandManager.literal(COMMAND_CONCISE_COORDS)
+	            .executes(context -> {
+                config.conciseCoords = !config.conciseCoords;
+                AshMod.configManager.save();
+                return 1;
+            }));
+		});
     }
 }
